321af232802b51e28ae5732be06fad74c6eeba85
Lucas Santiago <88990286+M6488@users.noreply.github.com>
2025-09-11 22:21:35 +0000

    fase 1

--- twilio_client.py
+++ twilio_client.py
@@ -1,6 +1,5 @@
 from twilio.rest import Client
 from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_WHATSAPP_FROM
-
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 def send_whatsapp(to_whatsapp: str, body: str):
