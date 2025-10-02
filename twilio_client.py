b06b15c749b3fc5f7d89a044e2976d50547ced4b
Lucas Santiago <88990286+M6488@users.noreply.github.com>
2025-10-02 17:24:09 +0000

    elhoria no twilio

--- twilio_client.py
+++ twilio_client.py
@@ -4,6 +4,10 @@ from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_WHATSAPP_FROM
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 def send_whatsapp(to_whatsapp: str, body: str):
+    
+    if to_whatsapp == TWILIO_WHATSAPP_FROM:
+        print(f"Ignorando mensagem: to ({to_whatsapp}) é igual ao from ({TWILIO_WHATSAPP_FROM})")
+        return None
 
     message = client.messages.create(
         from_=TWILIO_WHATSAPP_FROM,
